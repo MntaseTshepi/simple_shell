79e9987cf62faa69e6ad19c5961d39b965794b98
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-05-20 10:55:20 -0700

    Tokenize function update

--- tokenizePath.c
+++ tokenizePath.c
@@ -11,11 +11,13 @@ char **tokenizePath()
 	const char *delim = ":";
 	int num_token = 0, i = 0;
 	char *value_cpy = NULL, *val_copy = NULL, **PATH;
-	
+
 	value_cpy = _strdup(value);
 	if (value_cpy == NULL)
+	{
 		perror("sh: memory allocation failed");
 		exit(EXIT_FAILURE);
+	}
 	tokens = strtok(value_cpy, delim);
 	while (tokens != NULL)
 	{
@@ -25,9 +27,11 @@ char **tokenizePath()
 	num_token++;
 	PATH = malloc(sizeof(char *) * num_token);
 	if (PATH == NULL)
+	{
 		perror("sh: memory allocation failed");
 		free(PATH);
 		exit(EXIT_FAILURE);
+	}
 	val_copy = _strdup(value);
 	tokens = strtok(val_copy, delim);
 	for (i = 0; tokens != NULL; i++)
@@ -46,5 +50,5 @@ char **tokenizePath()
 	value_cpy = NULL;
 	free(val_copy);
 	val_copy = NULL;
-	return (PATH);
+	return (PATH);
 }
